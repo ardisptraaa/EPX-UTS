b2ffdf510b1b8c00bfd1a209f0ae4becd0415bf0
EPX-PANCA <rectapanca@gmail.com>
2017-11-20 15:22:24 +0700

    koding

--- app/src/main/java/com/example/ardi/pantheon/MainActivity.java
+++ app/src/main/java/com/example/ardi/pantheon/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.ardi.pantheon;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
 public class MainActivity extends AppCompatActivity {
 
 
@@ -11,6 +15,37 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
+    }package com.example.ardi.pantheon;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.content.Intent;
+    public class MainActivity extends AppCompatActivity {
+
+        ImageView imgView;
+        TextView txtView;
+        int linkGambar;
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+
+
+            Intent intent = getIntent();
+            String Tit = intent.getStringExtra("Tit");
+
+            setTitle(Tit);
+            linkGambar = intent.getIntExtra("gambar", linkGambar);
+            imgView.setImageResource(linkGambar);
+            txtView.setText(intent.getStringExtra("text"));
+
+
+        }
     }
+
+
 }
 
--- app/src/main/java/com/example/ardi/pantheon/SplashActivity.java
+++ app/src/main/java/com/example/ardi/pantheon/SplashActivity.java
@@ -1,17 +1,34 @@
 package com.example.ardi.pantheon;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Handler;
 import android.os.Bundle;
-
+import android.view.Window;
+import android.widget.TextView;
 
 
 public class SplashActivity extends Activity {
-
+    //  TextView tvSplash;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        //menghilangkan ActionBar
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_splash);
 
+        // tvSplash = (TextView) findViewById(R.id.tvSplash);
+
+
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplicationContext(), MenuOSActivity.class));
+                finish();
+            }
+        }, 3000L);
     }
 }
